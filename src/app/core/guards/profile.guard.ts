cff3dbf29cec40fbf642f878e926d9aff23cf2c7
JUANFALCONCODIGO <JUANCANEN_10_@HOTMAIL.COM>
2023-06-13 12:09:06 -0500

    [UPDATE] Updated configuration canActivate

--- src/app/core/guards/profile.guard.ts
+++ src/app/core/guards/profile.guard.ts
@@ -6,10 +6,10 @@ export const profileGuard = (): CanActivateFn => {
         const router=inject(Router)
         const isLogin = localStorage.getItem('isLogin');
         console.log('[isLogin]',isLogin);
-        if (isLogin && isLogin == 'true') {
-            return true;
+        if (isLogin && isLogin == 'false') {
+            return router.navigate(['/home']);
 
         }
-        return router.navigate(['/home']);
+        return true;
     };
 }
